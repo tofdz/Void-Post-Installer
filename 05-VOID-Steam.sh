8f54fb2c9aa8376745a52ef326ca8076965a1fd3
tofdz <80813116+tofdz@users.noreply.github.com>
2021-10-31 01:22:53 +0200

    Update 05-VOID-Steam.sh

--- 05-VOID-Steam.sh
+++ 05-VOID-Steam.sh
@@ -8,13 +8,8 @@ function BASE(){
 #Installe le necessaire pour faire fonctionner steam & gamepad
 flatpak install app/com.valvesoftware.Steam/x86_64/stable
 sudo vpm i -y libgcc-32bit libstdc++-32bit libdrm-32bit libglvnd-32bit mono void-repo-multilib{,-nonfree}
-sudo vpm i -y sc-controller
-sudo usermod -a -G input $USER 
-}
 
-function OPENSOURCE(){
-#installe les drivers opensource
-sudo vpm i -y mesa-dri-32bit
+sudo usermod -a -G input $USER 
 }
 
 function NVIDIA(){
@@ -22,10 +17,20 @@ function NVIDIA(){
 sudo vpm i -y nvidia-libs-32bit-470.74_1
 }
 
+function OPENSOURCE(){
+#installe les drivers opensource
+sudo vpm i -y mesa-dri-32bit
+}
+
+function CONTROLLER(){
+sudo vpm i -y sc-controller
+}
 function MAIN(){
+
 BASE
-#OPENSOURCE
 NVIDIA
+#OPENSOURCE
+CONTROLLER
 }
 
 MAIN
