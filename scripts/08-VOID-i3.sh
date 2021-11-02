5f1a85ffd4cee14b86a06dbe147916e5c1511a4f
tofdz <80813116+tofdz@users.noreply.github.com>
2021-11-02 18:58:58 +0100

    Update 08-VOID-i3.sh

--- scripts/08-VOID-i3.sh
+++ scripts/08-VOID-i3.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-I3CONF=$HOME/.config/i3/
-I3BCONF=$HOME/.config/i3/i3blocks.conf
+## VARIABLES
+
+I3CONF=$HOME/.config/i3				# Dossier .config/i3/
+I3FILE=$I3CONF/config				# Fichier .config/i3/config
 
 if [ ! -d $HOME/.fonts ];then
 	mkdir $HOME/.fonts
@@ -13,19 +15,18 @@ else
 echo "Dossier i3 déjà présent"
 fi
 
-# PREPARATION DES FICHIERS DE CONFIF ET CLEAN POUR L'INSTALL
-if [ -f $I3CONF/config ];then
-sudo pycp $I3CONF/config $I3CONF/config.ori
-rm -fv $I3CONF/config
-fi
 # INSTALLATION DES FICHIERS DE CONFIGURATION & WALLPAPERS
+sudo vpm i -y i3-gaps i3ipc-glib i3status i3wsr dmenu rofi i3blocks i3blocks-blocklets pasystray font-awesome5 lxappearance feh adwaita-plus compton ImageMagick i3lock-color xautolock 
+
+## PREPARATION DES FICHIERS DE CONFIF ET CLEAN POUR L'INSTALL
+
+# Verification
+if [ -f $I3FILE ];then
+sudo pycp $I3FILE $I3CONF/config.ori
+rm -fv $I3FILE
+fi
+
 sudo pycp $HOME/Void-Post-Installer/blurlock /bin/
-sudo pycp $HOME/Void-Post-Installer/config/config $I3CONF
-sudo pycp $HOME/Void-Post-Installer/config/i3blocks.conf $I3CONF
+sudo pycp $HOME/Void-Post-Installer/config/i3blocks.conf $HOME/Void-Post-Installer/config/config $I3CONF
 
 sudo chmod +x /bin/blurlock
-
-#Installer la police qui va bien
-#OUUUUUUUUUPPPP OUUUUUUUUUUP THAAAAAAAT'S DAAAAAAAAA SOUUUUUUUUUN OF DAAAAAAAAAA POOOOOOOOOLICE !
-
-sudo vpm i -y i3-gaps i3ipc-glib i3status i3wsr dmenu rofi i3blocks i3blocks-blocklets pasystray font-awesome5 lxappearance feh adwaita-plus compton ImageMagick i3lock-color xautolock 
