6d8c3f3193bd29222c90bce4de954e040b7388b0
tofdz <80813116+tofdz@users.noreply.github.com>
2021-11-02 16:26:04 +0100

    Update 08-VOID-i3.sh

--- scripts/08-VOID-i3.sh
+++ scripts/08-VOID-i3.sh
@@ -22,14 +22,10 @@ fi
 sudo pycp $HOME/Void-Post-Installer/blurlock /bin/
 sudo pycp $HOME/Void-Post-Installer/config/config $I3CONF
 sudo pycp $HOME/Void-Post-Installer/config/i3blocks.conf $I3CONF
-sudo pycp $HOME/Void-Post-Installer/wallpapers/* $HOME/Images/
+
+sudo chmod +x /bin/blurlock
 
 #Installer la police qui va bien
 #OUUUUUUUUUPPPP OUUUUUUUUUUP THAAAAAAAT'S DAAAAAAAAA SOUUUUUUUUUN OF DAAAAAAAAAA POOOOOOOOOLICE !
-cd $HOME
-git clone https://github.com/supermarin/YosemiteSanFranciscoFont
-sudo pycp $HOME/YosemiteSanFranciscoFont/*.ttf $HOME/.fonts/
-fc-cache -fv
-echo "Suppression des Fichiers inutile"
-rm -rfv YosemiteSanFranciscoFont
+
 sudo vpm i -y i3-gaps i3ipc-glib i3status i3wsr dmenu rofi i3blocks i3blocks-blocklets pasystray font-awesome5 lxappearance feh adwaita-plus compton ImageMagick i3lock-color 
