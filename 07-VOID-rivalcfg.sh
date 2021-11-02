c422f499af9f0b83e89dacd96c59ee47210a9543
tofdz <80813116+tofdz@users.noreply.github.com>
2021-11-02 16:31:56 +0100

    Delete 07-VOID-rivalcfg.sh

--- 07-VOID-rivalcfg.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-BASEDIR=$(pwd)
-cd $HOME
-git clone https://github.com/flozz/rivalcfg.git
-cd rivalcfg
-sudo pip3 install .
-sudo rivalcfg --update-udev
-sudo rivalcfg -p 125
-cd $BASEDIR
