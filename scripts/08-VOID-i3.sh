96e4e0ec7839c58d4ec454a805916b59768974ff
tofdz <80813116+tofdz@users.noreply.github.com>
2021-11-02 19:23:33 +0100

    Update 08-VOID-i3.sh

--- scripts/08-VOID-i3.sh
+++ scripts/08-VOID-i3.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 ## VARIABLES
-I3CONF=$HOME/.config/i3				# Dossier .config/i3/
-I3FILE=$I3CONF/config				# Fichier .config/i3/config
+CONFDIR=$HOME/.config/					# Dossier .config
+I3DIRCONF=$HOME/.config/i3				# Dossier .config/i3/
+I3FILE=$I3DIRCONF/config				# Fichier .config/i3/config
 
 if [ ! -d $HOME/.fonts ];then
 	mkdir $HOME/.fonts
 else
 echo "Dossier .fonts déjà présent"
 fi
-if [ ! -d $I3CONF ];then
-	mkdir $I3CONF
+if [ ! -d $I3DIRCONF ];then
+	mkdir $I3DIRCONF
 else
 echo "Dossier i3 déjà présent"
 fi
@@ -21,12 +22,12 @@ sudo vpm i -y i3-gaps i3ipc-glib i3status i3wsr dmenu rofi i3blocks i3blocks-blo
 
 # Verification
 if [ -f $I3FILE ];then
-sudo pycp $I3FILE $I3CONF/config.ori
+sudo pycp $I3FILE $I3DIRCONF/config.ori
 rm -fv $I3FILE
 fi
 
 sudo pycp $WDIR/scripts/blurlock /bin/
-sudo pycp $WDIR/config/* $I3CONF
+sudo pycp $WDIR/config/* $CONFDIR
 
 
 sudo chmod +x /bin/blurlock
