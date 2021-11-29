a4862be2a882fc844307253d563a9cc2ffd626bf
tofdz <80813116+tofdz@users.noreply.github.com>
2021-11-29 21:26:46 +0100

    Add files via upload

--- scripts/05-VOID-Steam.sh
+++ scripts/05-VOID-Steam.sh
@@ -8,7 +8,7 @@ function BASE(){
 #Installe le necessaire pour faire fonctionner steam & gamepad
 
 sudo vpm i -y steam libgcc-32bit libstdc++-32bit libdrm-32bit libglvnd-32bit mono
-sudo usermod -a -G input $USER 
+sudo usermod -a -G input $USER
 }
 
 function NVIDIA(){
@@ -22,9 +22,14 @@ sudo vpm i -y mesa-dri-32bit
 }
 function PROTONUP(){
 pip3 install protonup
+touch $HOME/.profile
+echo 'if [ -d "$HOME/.local/bin" ] ; then' > $HOME/.profile
+echo 'PATH="$HOME/.local/bin:$PATH"' >> $HOME/.profile
+echo "fi" >> $HOME/.profile
+source $HOME/.profile
 if [ ! -d ~/.steam/root/compatibilitytools.d ];then
-mkdir ~/.steam/root/compatibitytools.d/ && protonup -d "~/.steam/root/compatibilitytools.d/"
-protonup -d $HOME/.local/share/Steam/compatibilitytools.d/
+sudo mkdir -R ~/.steam/root/compatibitytools.d/ && protonup -d "~/.local/share/Steam/compatibilitytools.d/"
+protonup -d "$HOME/./local/share/Steam/compatibilitytools.d/"
 protonup -y
 fi
 }
