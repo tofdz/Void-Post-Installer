dd13c43ac8aae519dac28810901838a045e55221
tofdz <80813116+tofdz@users.noreply.github.com>
2021-12-10 08:22:01 +0100

    Add files via upload

--- install-GUI.sh
+++ install-GUI.sh
@@ -196,8 +196,7 @@ function T420(){
 
 echo "===> T420 addons"
 sudo vpm i -y tlp tlp-rdw tp_smapi-dkms tpacpi-bat mesa-dri linux-firmware-intel vulkan-loader mesa-vulkan-intel intel-video-accel libva-intel-driver
-# sudo cp lenovo-mutemic.sh /etc/acpi/&&sudo chmod +x /etc/acpi/lenovo-mutemic.sh;
-sudo chmod +x $HOME/Void-Post-Installer/outils/lenovo-mutemusic.sh;sudo pycp $HOME/Void-Post-Installer/outils/lenovo-mutemusic.sh /etc/acpi
+sudo chmod +x $HOME/Void-Post-Installer/lenovo/lenovo-mutemusic.sh;sudo pycp $HOME/Void-Post-Installer/lenovo/lenovo-mutemusic.sh /etc/acpi
 sudo vsv restart acpid
 }
 function X250(){
@@ -212,7 +211,7 @@ if [ -n $(sudo grep 'intel' /etc/default/grub) ];then
 else
 	echo "Fichier déjà modifié"
 fi
-sudo chmod +x $HOME/Void-Post-Installer/outils/lenovo-mutemusic.sh;sudo pycp $HOME/Void-Post-Installer/outils/lenovo-mutemusic.sh /etc/acpi
+sudo chmod +x $HOME/Void-Post-Installer/lenovo/lenovo-mutemusic.sh;sudo pycp $HOME/Void-Post-Installer/lenovo/lenovo-mutemusic.sh /etc/acpi
 sudo vsv restart acpid;
 sudo vpm i -y linux-firmware-broadcom linux-firmware-intel linux-firmware-network intel-ucode mesa-dri mesa-vulkan-intel
 sudo vpm i -y tp_smapi-dkms tpacpi-bat
