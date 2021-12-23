63389626723f05064139d1f20124e03bfdc2f476
tofdz <80813116+tofdz@users.noreply.github.com>
2021-12-23 12:55:26 +0100

    Add files via upload

--- scripts/05-VOID-Steam.sh
+++ scripts/05-VOID-Steam.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Installation de Steam sur VoidLinux.
-sudo vpm i -y steam libstdc++ libdrm libglvnd mono
+sudo vpm i -y steam libgcc-32bit libstdc++-32bit libdrm-32bit libglvnd-32bit mono
 sudo usermod -a -G input $USER
--- scripts/10-VOID-Wine.sh
+++ scripts/10-VOID-Wine.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Installation de Wine pour Voidlinux
 
-sudo vpm i -y wine wine-mono wine-tools winetricks vulkan-loader-32bit
+sudo vpm i -y wine wine-32bit wine-mono wine-tools winetricks vulkan-loader-32bit
 
