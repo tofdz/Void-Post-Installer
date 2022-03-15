cee059c874bf114084a57fe59cf8babc66f83097
tofdz <80813116+tofdz@users.noreply.github.com>
2022-03-16 00:52:22 +0100

    Update 05-VOID-Steam.sh

--- scripts/05-VOID-Steam.sh
+++ scripts/05-VOID-Steam.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-
+echo "==> Install Steam"
+sudo usermod -a -G input $USER
 # Installation de Steam sur VoidLinux.
 sudo vpm i -y steam libgcc-32bit libstdc++-32bit libdrm-32bit libglvnd-32bit mono
-sudo usermod -a -G input $USER
+
+# Installation de Steam via flatpak (EldenRing EAC actif)
+flatpak install com.valvesoftware.Steam org.freedesktop.Platform.VulkanLayer.MangoHud
+sudo -S echo "vm.max_map_count=1048576" >> etc/sysctl.conf
