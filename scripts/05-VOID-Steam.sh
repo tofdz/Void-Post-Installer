eb45776150b2176db95bad2f327d6d7da9acf2f8
tofdz <80813116+tofdz@users.noreply.github.com>
2022-03-30 15:02:46 +0200

    Update 05-VOID-Steam.sh

--- scripts/05-VOID-Steam.sh
+++ scripts/05-VOID-Steam.sh
@@ -6,4 +6,14 @@ sudo vpm i -y steam libgcc-32bit libstdc++-32bit libdrm-32bit libglvnd-32bit mon
 
 # Installation de Steam via flatpak (EldenRing EAC actif)
 flatpak --user install com.valvesoftware.Steam org.freedesktop.Platform.VulkanLayer.MangoHud
-sudo -S echo "vm.max_map_count=1048576" >> etc/sysctl.conf
+
+# Verification modification sur sysctl.conf
+
+	VERIF=$(cat /etc/sysctl.conf|grep vm.max_map_count=1048576)
+	echo "Vérification /etc/sysctl.con : $VERIF"
+	if [ -e $VERIF ];then
+	echo "Modification /etc/sysclt absente : modification en cours"
+	sudo -S echo "vm.max_map_count=1048576" >> etc/sysctl.conf
+	else
+	echo "Fichier /etc/sysctl.conf : déjà modifié"
+	fi
