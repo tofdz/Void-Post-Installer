5615594453f3d0ee6575a214557a360f7b371403
tofdz <80813116+tofdz@users.noreply.github.com>
2022-08-01 19:50:18 +0200

    Update 03-VOID-Login_AZERTY.sh

--- scripts/03-VOID-Login_AZERTY.sh
+++ scripts/03-VOID-Login_AZERTY.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
-if [ -d /etc/X11/xorg.conf.d ];then
-	echo -e "Dossier xorg.conf.d présent"
+sudo -S echo "===> ELOGIND AZERTY"
+if [ -d /etc/X11/xorg.conf.d ]; then
+	sudo -S echo -e "Dossier xorg.conf.d présent"
 else
-	sudo -S mkdir -R /etc/X11/xorg.conf.d/
-	echo -e "Dossier crée"
+	sudo -S mkdir /etc/X11/xorg.conf.d/
+	sudo -S echo -e "Dossier crée"
 fi
 				
-if [ -f /etc/X11/xorg.conf.d/00-Keyboard.conf ];then
-	echo -e "Fichier 00-Keyboard.conf présent"
+if [ -f /etc/X11/xorg.conf.d/00-Keyboard.conf ]; then
+	sudo -S echo -e "Fichier 00-Keyboard.conf présent"
+	
+	# TEST DU FICHIER (A FAIRE)
+	
 else
-sudo -S	touch /etc/X11/xorg.conf.d/00-Keyboard.conf
-	sudo -S	sh -c "echo 'Section "InputClass"' > /etc/X11/xorg.conf/00-Keyboard.conf"
-    sudo -S	sh -c "echo '     Identifier "system-keyboard"' >> /etc/X11/xorg.conf/00-Keyboard.conf"
-    sudo -S	sh -c "echo '     MatchisKeyboard "on"' >> /etc/X11/xorg.conf/00-Keyboard.conf"
-	sudo -S	sh -c "echo '     Option "XkbLayout" "fr"' >> /etc/X11/xorg.conf/00-Keyboard.conf"
-    sudo -S	sh -c "echo '     Option "XkbModel" "pc105"' >> /etc/X11/xorg.conf/00-Keyboard.conf"
-	sudo -S	sh -c "echo 'EndSection' >> /etc/X11/xorg.conf/00-Keyboard.conf"
-	sudo -S	echo 'Fichier créé'
+	sudo -S	touch /etc/X11/xorg.conf.d/00-Keyboard.conf
+	sudo -S	echo 'Section "InputClass' > /etc/X11/xorg.conf.d/00-Keyboard.conf
+    sudo -S	echo '     Identifier "system-keyboard"' >> /etc/X11/xorg.conf.d/00-Keyboard.conf
+    sudo -S	echo '     MatchisKeyboard "on"' >> /etc/X11/xorg.conf.d/00-Keyboard.conf
+	sudo -S	echo '     Option "XkbLayout" "fr"' >> /etc/X11/xorg.conf.d/00-Keyboard.conf
+    sudo -S	echo '     Option "XkbModel" "pc105"' >> /etc/X11/xorg.conf.d/00-Keyboard.conf
+	sudo -S	echo 'EndSection' >> /etc/X11/xorg.conf.d/00-Keyboard.conf
 fi
