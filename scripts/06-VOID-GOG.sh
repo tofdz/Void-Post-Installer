ed68a0b1751768000e1aca7342005d65f5c6f197
tofdz <80813116+tofdz@users.noreply.github.com>
2022-08-01 19:50:40 +0200

    Update 06-VOID-GOG.sh

--- scripts/06-VOID-GOG.sh
+++ scripts/06-VOID-GOG.sh
@@ -5,4 +5,4 @@
 
 #Liste des paquets de base pour install GOG sur VoidLinux
 
-sudo vpm i -y minigalaxy LGOGDownloader-3.8_1 python3-gogs-client-1.0.6_7
+sudo vpm i -y minigalaxy LGOGDownloader python3-gogs-client
