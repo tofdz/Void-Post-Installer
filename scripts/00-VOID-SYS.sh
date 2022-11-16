1c3243ae57ed79c9aa0b6dcefba1acdf9bfd9b9e
tofdz <80813116+tofdz@users.noreply.github.com>
2022-11-16 02:11:53 +0100

    Update 00-VOID-SYS.sh

--- scripts/00-VOID-SYS.sh
+++ scripts/00-VOID-SYS.sh
@@ -1,65 +1,59 @@
 #!/bin/bash
-voiduser=$USER
-
-
-echo "==> 00 Void : System modifications"
-echo "$voiduser"
-
-# CLEANINSTALL
-echo -e "==> CLEANER"
-#CLEANER
-echo "===> BASE CLEANING - PLEASE WAIT !"
-sudo -S touch /etc/xbps.d/Base_Install.conf
-sudo -S echo "ignorepkg=linux-firmware-amd" >> /etc/xbps.d/Base_Install.conf
-sudo -S echo "ignorepkg=linux-firmware-intel" >> /etc/xbps.d/Base_Install.conf
-sudo -S echo "ignorepkg=linux-firmware-nvidia" >> /etc/xbps.d/Base_Install.conf
-sudo -S echo "ignorepkg=xf86-video-amdgpu" >> /etc/xbps.d/Base_Install.conf
-sudo -S echo "ignorepkg=xf86-video-ati" >> /etc/xbps.d/Base_Install.conf
-sudo -S echo "ignorepkg=xf86-video-dummy" >> /etc/xbps.d/Base_Install.conf
-sudo -S echo "ignorepkg=xf86-video-fbdev" >> /etc/xbps.d/Base_Install.conf
-sudo -S echo "ignorepkg=xf86-video-intel" >> /etc/xbps.d/Base_Install.conf
-sudo -S echo "ignorepkg=xf86-video-nouveau" >> /etc/xbps.d/Base_Install.conf
-sudo -S echo "ignorepkg=xf86-video-vesa" >> /etc/xbps.d/Base_Install.conf
-sudo -S echo "ignorepkg=xf86-video-vmware" >> /etc/xbps.d/Base_Install.conf
-
-sudo vpm remove -y linux-firmware-amd linux-firmware-intel linux-firmware-nvidia xf86-video-amdgpu xf86-video-ati xf86-video-dummy xf86-video-fbdev xf86-video-intel xf86-video-nouveau xf86-video-vesa xf86-video-vmware 
-echo "==> Suppression Base_Install.conf"
-sudo -S rm /etc/xbps.d/Base_Install.conf
-
-# Verification /etc/sysctl.conf
-
+voiduser=$(echo $USER)
 VER1=$(cat /etc/sysctl.conf|grep vm.max_map_count=1048576)
 VER2=$(cat /etc/sysctl.conf|grep "abi.vsyscall32 = 0")
 VER4=$(cat /etc/security/limits.conf|grep '1048576')
 VER5=$(cat /etc/environment|grep -c 'qt5ct')
+sudo -s <<eof
+echo -e "==> 00 Void : System modifications"
 
+# CLEANINSTALL
+echo -e "==> CLEANER"
+
+#CLEANER
+echo "===> BASE CLEANING - PLEASE WAIT !"
+touch /etc/xbps.d/Base_Install.conf
+echo "ignorepkg=linux-firmware-amd" >> /etc/xbps.d/Base_Install.conf
+echo "ignorepkg=linux-firmware-intel" >> /etc/xbps.d/Base_Install.conf
+echo "ignorepkg=linux-firmware-nvidia" >> /etc/xbps.d/Base_Install.conf
+echo "ignorepkg=xf86-video-amdgpu" >> /etc/xbps.d/Base_Install.conf
+echo "ignorepkg=xf86-video-ati" >> /etc/xbps.d/Base_Install.conf
+echo "ignorepkg=xf86-video-dummy" >> /etc/xbps.d/Base_Install.conf
+echo "ignorepkg=xf86-video-fbdev" >> /etc/xbps.d/Base_Install.conf
+echo "ignorepkg=xf86-video-intel" >> /etc/xbps.d/Base_Install.conf
+echo "ignorepkg=xf86-video-nouveau" >> /etc/xbps.d/Base_Install.conf
+echo "ignorepkg=xf86-video-vesa" >> /etc/xbps.d/Base_Install.conf
+echo "ignorepkg=xf86-video-vmware" >> /etc/xbps.d/Base_Install.conf
+
+vpm remove -y linux-firmware-amd linux-firmware-intel linux-firmware-nvidia xf86-video-amdgpu xf86-video-ati xf86-video-dummy xf86-video-fbdev xf86-video-intel xf86-video-nouveau xf86-video-vesa xf86-video-vmware 
+echo "==> Suppression Base_Install.conf"
+rm /etc/xbps.d/Base_Install.conf
+
+# Verification /etc/sysctl.conf
 echo "==> Vérification /etc/sysctl.conf : $VER1"
-sudo -S echo "===> 02 /etc/sysctl.conf : vm.max_map_count=1048576"
+echo "===> 02 /etc/sysctl.conf : vm.max_map_count=1048576"
 if [ -e $VER1 ]; then
-	sudo -S echo "==!> Modification /etc/sysclt absente : modification en cours"
-	sudo -S echo "vm.max_map_count=1048576" >> /etc/sysctl.conf
-	VER1A=$(cat /etc/sysctl.conf|grep vm.max_map_count=1048576)
-	sudo -S echo "==> Vérification /etc/sysctl.conf : $VER1A"
+	echo "==!> Modification /etc/sysclt absente : modification en cours"
+	echo "vm.max_map_count=1048576" >> /etc/sysctl.conf
+	echo "==> Vérification /etc/sysctl.conf : $VER1"
 	else
-	sudo -S echo "==> Fichier /etc/sysctl.conf : déjà modifié"
+	echo "==> Fichier /etc/sysctl.conf : déjà modifié"
 fi
-
 sudo -S echo "===> 03 /etc/sysctl.conf : abi.vsyscall32 = 0"
 if [ -e $VER2 ]; then
 	sudo -S echo "Modification /etc/sysclt absente : modification en cours"
-	sudo -S echo "abi.vsyscall32 = 0" >> /etc/sysctl.conf
-	VER2A=$(cat /etc/sysctl.conf|grep "abi.vsyscall32 = 0")
-	sudo -S echo "Vérification /etc/sysctl.conf : $VER2A"
+	echo "abi.vsyscall32 = 0" >> /etc/sysctl.conf
+	sudo -S echo "Vérification /etc/sysctl.conf : $VER2"
 	else
 	sudo -S echo "Fichier /etc/sysctl.conf : déjà modifié"
 fi
 
-# VERIFICATION /etc/security/limits.conf
-sudo -S echo "===> 05 /etc/security/limits.conf"
+# Verification limits.conf
+sudo -S echo "===> 04 /etc/security/limits.conf pour $voiduser"
 if [ -e $VER4 ]; then
-	echo "==> Modification /etc/security/limits.conf"
-	sudo -S echo "@users           -       nice            -20" >> /etc/security/limits.conf
-	sudo -S echo "@users           -       nofile          1048576" >> /etc/security/limits.conf
+	sudo -S echo "==> Modification /etc/security/limits.conf"
+	echo "$voiduser           -       nice            -20" >> /etc/security/limits.conf
+	echo "$voiduser           -       nofile          1048576" >> /etc/security/limits.conf
 else
 	sudo -S echo "==> /etc/security/limits.conf déjà modifié"
 fi
@@ -67,74 +61,30 @@ fi
 # Modification /etc/environment
 if [ -e $VER5 ]; then
 sudo -S echo "==> Modification /etc/environment QT_QPA_PLATEFORMTHEME=qt5ct"
-sudo -S echo 'export QT_QPA_PLATFORMTHEME=qt5ct' >> /etc/environment
+echo 'export QT_QPA_PLATFORMTHEME=qt5ct' >> /etc/environment
 else
 sudo -S echo "PASS ==> /etc/environment déjà modifié"
 fi
-# Prendre en compte le $HOME/$USER/.local/bin en compte dans le $PATH
-sudo -S echo "===> $HOME/$USER/.local/bin : Modification fichiers"
+
+#==============================
+# PATH : $HOME/$USER/.local/bin
+#==============================
+echo "===> $HOME/.local/bin : Modification fichiers"
 if [ -e $(cat /etc/profile|grep "/.local/bin") ]; then
 	sudo -S echo "==> /etc/profile : Modification en cours ..."
-	sudo -S echo 'if [ -d "$HOME/.local/bin" ]; then' >> /etc/profile
-	sudo -S echo 'PATH=$HOME/.local/bin:$PATH' >> /etc/profile
-	sudo -S echo 'fi' >> /etc/profile
-	sudo -S echo 'if [ -d /var/lib/flatpak/exports/share ]; then' >> /etc/profile
-	sudo -S echo 'PATH=/var/lib/flatpak/exports/share:$PATH' >> /etc/profile
-	sudo -S echo 'fi' >> /etc/profile
-	sudo -S echo 'if [ -d $HOME/.local/share/flatpak/exports/share ]; then'  >> /etc/profile
-	sudo -S echo 'PATH=$HOME/.local/share/flatpak/exports/share:$PATH' >> /etc/profile
-	sudo -S echo 'fi' >> /etc/profile
+	echo 'if [ -d "$HOME/.local/bin" ]; then' >> /etc/profile
+	echo 'PATH=$HOME/.local/bin:$PATH' >> /etc/profile
+	echo 'fi' >> /etc/profile
+	echo 'if [ -d /var/lib/flatpak/exports/share ]; then' >> /etc/profile
+	echo 'PATH=/var/lib/flatpak/exports/share:$PATH' >> /etc/profile
+	echo 'fi' >> /etc/profile
+	echo 'if [ -d $HOME/.local/share/flatpak/exports/share ]; then'  >> /etc/profile
+	echo 'PATH=$HOME/.local/share/flatpak/exports/share:$PATH' >> /etc/profile
+	echo 'fi' >> /etc/profile
 	sudo -S echo 'Fichier profile - Terminé !'
-	sudo -S source /etc/profile
 else
-	sudo -S echo -e "==> /etc/profile : Fichier /etc/profile déjà modifié : "
-	sudo -S echo -e "==> /etc/profile : TERMINE"
+	echo -e "==> /etc/profile : Fichier /etc/profile déjà modifié : "
+	echo -e "==> /etc/profile : TERMINE"
 fi
 
-# AJOUT DE L'AUTO UPDATER & CONFIGURATION
-if [ ! -d /var/service/snooze-daily ]; then
-	if [ ! -d /etc/cron.daily ]; then
-	echo -e "Repertoire cron.daily absent : création en cours..."
-	sudo -S mkdir /etc/cron.daily
-	else
-	echo -e "Repertoire Cron.daily présent"
-	fi
-	echo -e "Création service snooze-daily en cours ..."
-	sudo -S ln -s /etc/sv/snooze-daily /var/service
-else
-	echo -e "Service snooze-daily déjà présent"
-fi
-pycp $WDIR/outils/VOID-UPDATER.sh $HOME/.local/bin/;
-chmod +x $HOME/.local/bin/VOID-UPDATER.sh;
-if [ ! -f /etc/cron.hourly/update ];then
-	sudo -S echo -e '#!/bin/bash' > /etc/cron.hourly/update
-	sudo -S echo -e "cd /home/$voiduser/" >> /etc/cron.hourly/update
-	sudo -S echo -e 'exec ./VOID-UPDATER.sh' >> /etc/cron.hourly/update
-	sudo -S chmod +x /etc/cron.hourly/update
-else
-	echo -e "Fichier deja présent"
-fi
-# AJOUT DE LA FONCTION RECHERCHE DANS THUNAR
-sudo -S echo "<?xml version="1.0" encoding="UTF-8"?>" > $HOME/.config/Thunar/uca.xml
-sudo -S echo "<actions>" >> $HOME/.config/Thunar/uca.xml
-sudo -S echo "<action>" >> $HOME/.config/Thunar/uca.xml
-sudo -S echo "	<icon>utilities-terminal</icon>" >> $HOME/.config/Thunar/uca.xml
-sudo -S echo "	<name>Ouvrir un terminal ici</name>" >> $HOME/.config/Thunar/uca.xml
-sudo -S echo "	<unique-id>1659223755776079-1</unique-id>" >> $HOME/.config/Thunar/uca.xml
-sudo -S echo "	<command>exo-open --working-directory %f --launch TerminalEmulator</command>" >> $HOME/.config/Thunar/uca.xml
-sudo -S echo "	<description>Exemple d’une action personnalisée</description>" >> $HOME/.config/Thunar/uca.xml
-sudo -S echo "	<patterns>*</patterns>" >> $HOME/.config/Thunar/uca.xml
-sudo -S echo "	<startup-notify/>" >> $HOME/.config/Thunar/uca.xml
-sudo -S echo "	<directories/>" >> $HOME/.config/Thunar/uca.xml
-sudo -S echo "</action>" >> $HOME/.config/Thunar/uca.xml
-sudo -S echo "<action>" >> $HOME/.config/Thunar/uca.xml
-sudo -S echo "	<icon>searching</icon>" >> $HOME/.config/Thunar/uca.xml
-sudo -S echo "	<name>Recherche</name>" >> $HOME/.config/Thunar/uca.xml
-sudo -S echo "	<unique-id>1663781577501216-1</unique-id>" >> $HOME/.config/Thunar/uca.xml
-sudo -S echo "	<command>/usr/bin/catfish --path=%f</command>" >> $HOME/.config/Thunar/uca.xml
-sudo -S echo "	<description>Effectuer une recherche</description>" >> $HOME/.config/Thunar/uca.xml
-sudo -S echo "	<patterns>*</patterns>" >> $HOME/.config/Thunar/uca.xml
-sudo -S echo "	<directories/>" >> $HOME/.config/Thunar/uca.xml
-sudo -S echo "	<text-files/>" >> $HOME/.config/Thunar/uca.xml
-sudo -S echo "</action>" >> $HOME/.config/Thunar/uca.xml
-sudo -S echo "</actions>" >> $HOME/.config/Thunar/uca.xml
+eof
